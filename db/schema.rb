e4a188e1c6c60e555866b9b2675de503590879c4
Avishek Kumar <avishek.kumar1@greatlearning.in>
2022-08-21 22:23:58 +0530

    created user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_17_080615) do
+ActiveRecord::Schema.define(version: 2022_08_21_164630) do
 
   create_table "todos", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 2022_08_17_080615) do
     t.datetime "updated_at", null: false
     t.boolean "completed", default: false
     t.boolean "is_deleted", default: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end

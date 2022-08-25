5a9b4f61898c9d7756016e6fd11b3ab23d228282
Avishek Kumar <avishek.kumar1@greatlearning.in>
2022-08-26 01:25:17 +0530

    add password field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_23_123451) do
+ActiveRecord::Schema.define(version: 2022_08_24_175843) do
 
   create_table "todos", force: :cascade do |t|
     t.string "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2022_08_23_123451) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end

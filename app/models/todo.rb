68b61ff15a599889913a1f593380efcc876b46f8
Avishek Kumar <avishek.kumar1@greatlearning.in>
2022-09-10 13:25:41 +0530

    linked comment and todo table

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,5 +1,7 @@
 class Todo < ApplicationRecord
     belongs_to :user
+    has_many :comments
+    
     validates :title, presence: true, length: {minimum: 6, maximum: 30}
     validates :description, presence: true, length: {minimum: 10, maximum:500}
 end

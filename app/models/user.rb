e5e3a46d21a2988cc7aa5531cee379e420e5243e
Avishek Kumar <avishek.kumar1@greatlearning.in>
2022-09-16 01:51:52 +0530

    added user id column in comment table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_many :todos
+    has_many :comments
     before_save { self.email = email.downcase }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 
